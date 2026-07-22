79f1220f6813a68b8d3e81f176920eaa45cb6c52
TonyKennah <tonykennah@hotmail.com>
2026-07-22 14:49:49 +0100

    mis spelling update

--- src/Tricasts.jsx
+++ src/Tricasts.jsx
@@ -18,7 +18,7 @@ const HOT_TRAINERS = [
   "Neil Mulholland", "Andrew Balding", "Tony Carroll", "Dan Skelton", "Richard Hannon",
   "Joseph Patrick O'Brien", "William Haggas", "Henry De Bromhead", "Gordon Elliott",
   "Lucinda Russell & Michael Scudamore", "Tim Easterby", "Richard & Peter Fahey",
-  "Charlie Appleby", "Martin Keighley", "Ben Pauling", "Jonjo & A J O'Neill"
+  "Charlie Appleby", "Martin Keighley", "Ben Pauling", "Jonjo & A.J. O'Neill"
 ];
 
 function Tricasts() {
